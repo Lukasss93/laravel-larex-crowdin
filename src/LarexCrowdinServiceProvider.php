55bc17d89c8fea9eec45cd4edbc6f03726f9b23c
Luca Patera <lucapatera@outlook.it>
2021-07-31 20:57:19 +0200

    Added Laravel provider + config

---
+++ src/LarexCrowdinServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lukasss93\LarexCrowdin;
+
+use CrowdinApiClient\Crowdin;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class LarexCrowdinServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/config/larex-crowdin.php' => config_path('larex-crowdin.php'),
+        ], 'larex-crowdin-config');
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/larex-crowdin.php', 'larex-crowdin');
+
+        $this->app->singleton(Crowdin::class, function(Application $app){
+            return new Crowdin([
+                'access_token' => config('larex-crowdin.token'),
+            ]);
+        });
+
+        $this->app->alias(Crowdin::class, 'crowdin');
+    }
+}
---
+++ src/config/larex-crowdin.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+     |--------------------------------------------------------------------------
+     | Crowdin API Token
+     |--------------------------------------------------------------------------
+     |
+     | Enter your Crowdin Personal Access Token here.
+     | You can generate your token at this url: https://crowdin.com/settings#api-key
+     |
+     | Please note: this library supports only the Crowdin API v2
+     |
+     */
+
+    'token' => env('LAREX_CROWDIN_TOKEN'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Crowdin Project Id
+     |--------------------------------------------------------------------------
+     |
+     | Enter your Crowdin Project Id here.
+     | You can get your project id (API v2) at this url: https://crowdin.com/project/your-project/settings#api
+     |
+     */
+
+    'project_id' => (int)env('LAREX_CROWDIN_PROJECT_ID'),
+
+];
